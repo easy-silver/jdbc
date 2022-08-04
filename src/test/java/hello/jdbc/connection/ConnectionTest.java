c2fdc831499c68e86cf090a2d0a10aff7e77fdf6
Timo <developer.timo@gmail.com>
2022-08-04 16:58:39 +0900

    [커넥션풀과 데이터소스 이해] DataSource 예제2 - 커넥션 풀
    - HikariCP를 통해 커넥션 풀에 데이터베이스 커넥션을 담는 과정 실습

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -29,6 +30,21 @@ public class ConnectionTest {
         //DataSource를 생성하는 시점에만 URL, USERNAME, PWD 필요(설정과 사용의 분리 가능)
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         useDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        //커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        //별도의 쓰레드를 사용하여 커넥션 풀에 커넥션을 채우기 때문에 결과를 확인하기 위해 메인 쓰레드를 잠시 대기시킨다.
+        Thread.sleep(1000);
     }
 
     private void useDataSource(DataSource dataSource) throws SQLException {
