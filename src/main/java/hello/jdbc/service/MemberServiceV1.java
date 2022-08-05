a98570c18a69e5e62a191958db30823bfcb18356
Timo <developer.timo@gmail.com>
2022-08-05 15:12:36 +0900

    [트랜잭션 이해] 트랜잭션 - 적용1
    - 정상 이체 상황에 대한 구현 및 검증

---
+++ src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -0,0 +1,28 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.SQLException;
+
+@RequiredArgsConstructor
+public class MemberServiceV1 {
+
+    private final MemberRepositoryV1 memberRepository;
+
+    public void accountTransfer(String formId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(formId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromMember.getMemberId(), fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toMember.getMemberId(), toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체 중 예외 발생!!!");
+        }
+    }
+}
