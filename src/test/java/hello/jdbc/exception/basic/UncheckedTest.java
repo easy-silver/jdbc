a5f22554c17ed771492377c9fc865f31f5619a9f
Timo <developer.timo@gmail.com>
2022-08-08 12:03:04 +0900

    [자바 예외 이해] 언체크 예외 기본 이해
    - UnCheckedException에 대해 예외를 던지는 케이스 작성, 검증

--- src/test/java/hello/jdbc/exception/basic/UncheckedTest.java
+++ src/test/java/hello/jdbc/exception/basic/UncheckedTest.java
@@ -1,6 +1,7 @@
 package hello.jdbc.exception.basic;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
@@ -10,6 +11,13 @@ public class UncheckedTest {
     void unchecked_catch() {
         Service service = new Service();
         service.callCatch();
+    }
+
+    @Test
+    void unchecked_throw() {
+        Service service = new Service();
+        Assertions.assertThatThrownBy(service::callThrow)
+                .isInstanceOf(MyUncheckedException.class);
     }
 
     /**
@@ -39,6 +47,13 @@ public class UncheckedTest {
             }
         }
 
+        /**
+         * 예외를 잡지 않아도 된다. 자연스럽게 상위로 넘어간다.
+         * 체크 예외와 다르게 throws 예외 선언을 하지 않아도 된다.(생략해도 선언된 것처럼 작동)
+         */
+        public void callThrow() {
+            repository.call();
+        }
     }
 
     static class Repository {
